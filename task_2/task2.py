c50b25328fea705cc539eb4a41e4c33a80a8a198
Oskar Sałata <Xpleew16@gmail.com>
2026-03-19 19:21:46 +0100

    added plane to display points on it

--- task_2/task2.py
+++ task_2/task2.py
@@ -6,3 +6,14 @@ def generate_random_points(number_of_points: int) -> list[list[int]]:
     for i in range(number_of_points):
         points_list.append([random.randrange(-20, 20), random.randrange(-20,20)])
     return points_list
+
+
+def show_points_on_plane(points:list[list[int]]):
+    x_coordinates = [x[0] for x in points]
+    y_coordinates = [x[1] for x in points]
+    plt.scatter(x_coordinates, y_coordinates)
+    plt.show()
+
+list_points = generate_random_points(10)
+print(list_points)
+show_points_on_plane(list_points)
