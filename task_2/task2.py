fc3d342169fbfaf3b026d0a8de66e3e1a59b2d23
Oskar Sałata <Xpleew16@gmail.com>
2026-03-19 18:54:20 +0100

    created method to generate random points for travelling salesman problem

--- task_2/task2.py
+++ task_2/task2.py
@@ -2,4 +2,7 @@ from matplotlib import pyplot as plt
 import random 
 
 def generate_random_points(number_of_points: int) -> list[list[int]]:
-    return [[0,0]]
+    points_list = []
+    for i in range(number_of_points):
+        points_list.append([random.randrange(-20, 20), random.randrange(-20,20)])
+    return points_list
