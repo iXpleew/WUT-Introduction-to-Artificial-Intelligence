6d57a3eb4792ff08c942361eff3c878073d3853c
Oskar Sałata <Xpleew16@gmail.com>
2026-03-20 21:57:17 +0100

    if you read this u a gay

--- task_2/task2.py
+++ task_2/task2.py
@@ -9,9 +9,11 @@ def generate_random_points(number_of_points: int) -> list[list[int]]:
 
 
 def show_points_on_plane(points:list[list[int]]):
+    
+    random.shuffle(points)
     x_coordinates = [x[0] for x in points]
     y_coordinates = [x[1] for x in points]
-    
+
     # for connecting all points
     x_coordinates.append(x_coordinates[0])
     y_coordinates.append(y_coordinates[0])
@@ -19,6 +21,5 @@ def show_points_on_plane(points:list[list[int]]):
     plt.plot(x_coordinates, y_coordinates)
     plt.show()
 
-list_points = generate_random_points(10)
-print(list_points)
+list_points = [[-14, 8], [-4, 17], [13, -10], [-11, -12], [-4, 13], [-20, -12], [-4, 9], [-12, 18], [-4, -2], [-16, 11]]
 show_points_on_plane(list_points)
