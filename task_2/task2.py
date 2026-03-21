ecc23e87ad33ef061355a42d20874db539eb11f5
Oskar Sałata <Xpleew16@gmail.com>
2026-03-21 11:31:58 +0100

    added shuffle_list method to save shuffle_number of list given

--- task_2/task2.py
+++ task_2/task2.py
@@ -8,9 +8,19 @@ def generate_random_points(number_of_points: int) -> list[list[int]]:
         points_list.append([random.randrange(-20, 20), random.randrange(-20,20)])
     return points_list
 
+
 def calculate_distance(point_a: list[int], point_b: list[int]) -> int:
     distamce = math.sqrt((point_b[0]-point_a[0])**2 + (point_b[1]-point_a[1])**2)
     return 0
+
+
+def shuffle_list(points:list[list[int]], shuffle_number: int) -> list[list[list[int]]]:
+    combination_list = []
+    for i in range(shuffle_number):
+        combination_list.append(random.shuffle(points))
+    return combination_list
+
+
 def show_points_on_plane(points:list[list[int]]):
     
     random.shuffle(points)
