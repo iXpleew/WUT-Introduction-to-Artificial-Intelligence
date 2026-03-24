06c59d3481676da6f10ddefbaedc65bc3e52d926
Oskar Sałata <Xpleew16@gmail.com>
2026-03-24 22:24:15 +0100

    added exponential distribution

--- task_2/task2.py
+++ task_2/task2.py
@@ -56,10 +56,14 @@ def select_random_path(shuffled_list: list[list[list[int]]], max_value: float) -
     return shuffled_list[-1]
 
 
-def roullete_selection(shuffled_list: list[list[list[int]]]) -> list[list[list[int]]]:
+def calculate_distribution(path_lenght: float, sigma: float) -> float:
+    return math.exp(sigma/path_lenght)
+
+
+def roullete_selection(shuffled_list: list[list[list[int]]], sigma: float, population_size: int) -> list[list[list[int]]]:
     survivors = []
-    roullete_maximum = sum([(1/calculate_total_distance(x)) for x in shuffled_list])
-    for i in range(50):
+    roullete_maximum = sum([calculate_distribution(calculate_total_distance(x), sigma) for x in shuffled_list])
+    for i in range(population_size//2):
         survivors.append(select_random_path(shuffled_list, roullete_maximum))
     return survivors
 
@@ -108,7 +112,7 @@ def add_crossovers(survivors: list[list[list[int]]]) -> list[list[list[int]]]:
 def add_mutations(new_generation: list[list[list[int]]]) -> list[list[list[int]]]:
     mutated_generation = []
     for path in new_generation:
-        if random.randint(1, 100) < 10:
+        if random.randint(1, 100) < 30:
             point_a = random.randint(1, len(path) - 1)
             point_b = random.randint(1, len(path) - 1)
             path[point_a], path[point_b] = path[point_b], path[point_a]
@@ -128,11 +132,11 @@ def show_points_on_plane(points:list[list[int]], generation_number: int):
     plt.show()
 
 
-def optimize_path(points:list[list[int]]):
-    survivors = shuffle_list(points, 100)
+def optimize_path(points:list[list[int]], population: int):
+    survivors = shuffle_list(points, population)
     counter = 0
     for _ in range(100):
-        survivors = roullete_selection(survivors)
+        survivors = roullete_selection(survivors, 1.2, population_size=population)
         survivors = add_crossovers(survivors)
         survivors = add_mutations(survivors)
         counter += 1
@@ -143,4 +147,4 @@ def optimize_path(points:list[list[int]]):
 
 if __name__ == "__main__":
     list_points = [[-14, 8], [20, 17], [13, -10], [-11, -12], [-4, 13], [-20, -12], [-4, 9], [-12, 18], [-4, -2], [-16, 11], [-3, 20], [-19, 19], [5, 0], [0, 13], [-9, -18]]
-    optimize_path(list_points)
+    optimize_path(list_points, population=100)
