c29d2c6ec80c873ee5d8d4b50a9ab0f603e7843e
Oskar Sałata <Xpleew16@gmail.com>
2026-03-28 20:58:46 +0100

    modified window

--- own_projects/sudoku_solver/screen_adjustment.py
+++ own_projects/sudoku_solver/screen_adjustment.py
@@ -4,4 +4,7 @@ def switch_window_on():
     root = tk.Tk()
     root.title("Sudoku solver by iXpleew")
     root.geometry("400x400")
+
+    sudoku_label = tk.Label(root, text="Sudoku solver, enter sample numbers: ")
+    sudoku_label.pack()
     root.mainloop()
--- own_projects/sudoku_solver/sudoku_solver.py
+++ own_projects/sudoku_solver/sudoku_solver.py
@@ -1,6 +1,9 @@
 import screen_adjustment as gui_file
 
+
 def main():
     gui_file.switch_window_on()
+
+
 if __name__=="__main__":
     main()
