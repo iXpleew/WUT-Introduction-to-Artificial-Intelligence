97c3cb1406c7ce4f697d61b923d417cf8d041813
Oskar Sałata <Xpleew16@gmail.com>
2026-03-28 20:44:14 +0100

    added file for window managment

---
+++ own_projects/sudoku_solver/screen_adjustment.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+def switch_window_on():
+    root = tk.Tk()
+    root.title("Sudoku solver by iXpleew")
+    root.geometry("400x400")
+    root.mainloop()
--- own_projects/sudoku_solver/sudoku_solver.py
+++ own_projects/sudoku_solver/sudoku_solver.py
@@ -1,5 +1,6 @@
-def main():
-    print("welcome to sudoku solver")
+import screen_adjustment as gui_file
 
+def main():
+    gui_file.switch_window_on()
 if __name__=="__main__":
     main()
