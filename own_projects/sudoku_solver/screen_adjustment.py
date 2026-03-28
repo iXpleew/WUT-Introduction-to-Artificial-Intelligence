8a89cc36612378e4c3ae6518f079b539c2481b17
Oskar Sałata <Xpleew16@gmail.com>
2026-03-28 21:30:08 +0100

    added squares

--- own_projects/sudoku_solver/screen_adjustment.py
+++ own_projects/sudoku_solver/screen_adjustment.py
@@ -3,8 +3,22 @@ import tkinter as tk
 def switch_window_on():
     root = tk.Tk()
     root.title("Sudoku solver by iXpleew")
-    root.geometry("400x400")
+    root.geometry("600x600")
 
     sudoku_label = tk.Label(root, text="Sudoku solver, enter sample numbers: ")
     sudoku_label.pack()
+
+    current_x = 135
+    current_y = 40
+    for i in range(9):
+        if i % 3 == 0:
+            current_y += 20
+        for j in range(9):
+            if j % 3 == 0:
+                current_x += 15
+            field = tk.Text(root, height=2, width=2, bg="gray")
+            field.place(x=current_x, y=current_y)
+            current_x += 30
+        current_y += 40
+        current_x = 135
     root.mainloop()
