4c4b290c32538efea9749d010d441bf913f4896a
Oskar Sałata <Xpleew16@gmail.com>
2026-04-24 19:42:51 +0200

    added math import

--- task_4/main.py
+++ task_4/main.py
@@ -1,26 +1,24 @@
 from matplotlib import pyplot as plt
 import pandas as pd 
 import numpy as np
+import math
 
 
-def get_csv_data(file_name: str) -> list[pd.DataFrame]:
+def get_csv_data(file_name: str) -> pd.DataFrame:
     ttt_info = pd.read_csv(file_name).sample(frac=1)
-    file_lenght = len(ttt_info)
-    training_border = int(file_lenght * 7/10)
-
-    training_data = ttt_info[:training_border]
-    testing_data = ttt_info[training_border:]
-    return [training_data, testing_data]
+    return ttt_info
 
 
 def calculate_entropy(data_set: pd.DataFrame, target_column: pd.Series):
-    pass
+    row_number = len(data_set)
+    target_values = data_set[target_column.name].unique()
+
+    entropy = 0
+    return entropy
 
 
 if __name__=="__main__":
-    df_training, df_testing = get_csv_data("tic+tac+toe+endgame/tic-tac-toe.data")
-    print(f"{df_training.head()} has a lenght of {len(df_training)}")
-    print("Now it's time for test:")
-    print(f"{df_testing.head()} has lenght of {len(df_testing)}")
+    provided_data = get_csv_data("tic+tac+toe+endgame/tic-tac-toe.data")
+    calculate_entropy(provided_data, provided_data[-1])
 
     
