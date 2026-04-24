485b9f29edd09bb57440cf61d22996a99024a059
Oskar Sałata <Xpleew16@gmail.com>
2026-04-24 11:31:32 +0200

    added get_csv_data method to read csv file

--- task_4/main.py
+++ task_4/main.py
@@ -3,5 +3,9 @@ import pandas as pd
 import numpy as np
 
 
+def get_csv_data(file_name: str) -> pd.DataFrame:
+    ttt_info = pd.read_csv(file_name)
+    return ttt_info
+
 if __name__=="__main__":
-    pass
+    df = get_csv_data("tic+tac+toe+endgame/tic-tac-toe.data")
