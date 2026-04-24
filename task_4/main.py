e1144407a0a581b9cfb9a96a77c7717b0331301a
Oskar Sałata <Xpleew16@gmail.com>
2026-04-24 20:03:51 +0200

    fixed bug with dataframe by adding .iloc() method

--- task_4/main.py
+++ task_4/main.py
@@ -14,11 +14,16 @@ def calculate_entropy(data_set: pd.DataFrame, target_column: pd.Series):
     target_values = data_set[target_column.name].unique()
 
     entropy = 0
+    for value in target_values:
+        value_count = len(data_set[data_set[target_column.name] == value])
+        proportion = value_count/row_number
+        entropy -= proportion * math.log2(proportion)
     return entropy
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     provided_data = get_csv_data("tic+tac+toe+endgame/tic-tac-toe.data")
-    calculate_entropy(provided_data, provided_data[-1])
+    data_set_entropy = calculate_entropy(provided_data, provided_data.iloc[:, -1])
+    print(f"Entropy of data set is {data_set_entropy}")
 
     
