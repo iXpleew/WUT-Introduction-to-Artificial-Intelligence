c63b56ff313b2532dc820d44c434035d1049183f
Oskar Sałata <Xpleew16@gmail.com>
2026-04-24 11:45:26 +0200

    added split between training set and validation set

--- task_4/main.py
+++ task_4/main.py
@@ -3,9 +3,20 @@ import pandas as pd
 import numpy as np
 
 
-def get_csv_data(file_name: str) -> pd.DataFrame:
+def get_csv_data(file_name: str) -> list[pd.DataFrame]:
     ttt_info = pd.read_csv(file_name)
-    return ttt_info
+    file_lenght = len(ttt_info)
+    training_border = int(file_lenght * 7/10)
+
+    training_data = ttt_info[:training_border]
+    testing_data = ttt_info[training_border:]
+    return [training_data, testing_data]
+
 
 if __name__=="__main__":
-    df = get_csv_data("tic+tac+toe+endgame/tic-tac-toe.data")
+    df_training, df_testing = get_csv_data("tic+tac+toe+endgame/tic-tac-toe.data")
+    print(f"{df_training.head()} has a lenght of {len(df_training)}")
+    print("Now it's time for test:")
+    print(f"{df_testing.head()} has lenght of {len(df_testing)}")
+
+    
