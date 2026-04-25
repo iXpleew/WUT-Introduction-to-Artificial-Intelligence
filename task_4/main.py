5d0902909d6f2960847f9a1302cc6e48e062ce90
Oskar Sałata <Xpleew16@gmail.com>
2026-04-25 12:38:15 +0200

    added calculate_information_gain() method

--- task_4/main.py
+++ task_4/main.py
@@ -21,9 +21,24 @@ def calculate_entropy(data_set: pd.DataFrame, target_column: pd.Series):
     return entropy
 
 
+def calculate_information_gain(data_set: pd.DataFrame, feature: pd.Series, target_column: pd.Series) -> float:
+    parent_entropy = calculate_entropy(data_set, target_column)
+    unique_values = data_set[feature.name].unique()
+    weighted_entropy = 0
+
+    for value in unique_values:
+        subset = data_set[data_set[feature.name] == value]
+        proportion = len(subset)/len(data_set)
+        weighted_entropy += proportion * calculate_entropy(subset, target_column)
+    information_gain = parent_entropy - weighted_entropy
+    return information_gain
+
+
 if __name__ == "__main__":
     provided_data = get_csv_data("tic+tac+toe+endgame/tic-tac-toe.data")
     data_set_entropy = calculate_entropy(provided_data, provided_data.iloc[:, -1])
     print(f"Entropy of data set is {data_set_entropy}")
 
-    
+    for i in range(9):
+        curr_info_gain = calculate_information_gain(provided_data, provided_data.iloc[:, i], provided_data.iloc[:, -1])
+        print(f"Information Gain for {i} index is {curr_info_gain}")
