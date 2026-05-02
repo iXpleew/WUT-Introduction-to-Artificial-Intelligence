e83ac1d690c59f2cb8dd5f19d8f15addb8dbd3d1
Oskar Sałata <Xpleew16@gmail.com>
2026-05-02 10:35:19 +0200

    started to work on a check correctness method in sudoku solver file

---
+++ own_projects/sudoku_solver/sudoku_solver.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+def check_correctness(position: list[list[tk.Text]]) -> bool:
+    parser_position = []
+    for index, board in enumerate(position):
+        parser_position.append([])
+        for text_field in board:
+            number = text_field.get('1.0', 'end-1c')
+            print(number)
+    return False
